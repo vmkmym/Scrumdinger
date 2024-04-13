e5cc89db79cbec107e98a71299ef4326f9cfd3e1
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-14 01:11:36 +0900

    feat: Displaying data in a list
    - key path를 initializer에 전달면 id가 설정됨.
    - WindowGroupSwiftUI가 제공하는 기본 Scene중 하나이다. 장치의 전체 화면을 채우는 창에 표시된다. 그래서 사용자가 앱을 시작하면 가장 먼저 보여지는 창이다.
    
    (trouble shooting)
    - Logging Error: Failed to initialize logging system. Log messages may be missing. If this issue persists, try setting IDEPreferLogStreaming=YES in the active scheme actions environment variables. -> 해결
    - No color named 'poppy', 'orange', 'yellow' found in asset catalog for main bundle -> 이유를 모르겠음

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -6,12 +6,23 @@
 //
 
 import Foundation
-
-struct DailyScrum {
+    
+// 프로토콜 준수 선언
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    // 기본값을 할당하는 초기화 프로그램 추가 -> 매개변수의 기본값을 정의하면 초기화 프로그램이나 함수를 호출할 때 해당 매개변수를 생략할 수 있음
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+            self.id = id
+            self.title = title
+            self.attendees = attendees
+            self.lengthInMinutes = lengthInMinutes
+            self.theme = theme
+        }
 }
 
 extension DailyScrum {
