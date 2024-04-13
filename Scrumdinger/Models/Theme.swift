be3d0656ad28ae5bcc9037672be20be21b4c8da8
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-13 23:43:32 +0900

    feat: Add TrailingIconLabelStyle for SwiftUI labels
    - TrailingIconLabelStyle을 정의하는 파일

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -5,4 +5,36 @@
 //  Created by MorinBeck on 4/13/24.
 //
 
-import Foundation
+import SwiftUI
+
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    // self값에 따라 .black or .white를 반환하는 .accentColor 속성을 추가한다.
+    // 강조 색상은 테마의 기본 색상에 대한 고대비 보완을 제공하여 뷰에 계속 액세스할 수 있도록 보장한다.
+    var accentColor: Color {
+            switch self {
+            case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+            case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+            }
+        }
+    // mainColor라는 Color 속성은 열거형의 rawValue를 사용하여 컬러를 생성한다.
+    var mainColor: Color {
+            Color(rawValue)
+        }
+}
--- Scrumdinger/TrailingIconLabelStyle.swift
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -5,4 +5,18 @@
 //  Created by MorinBeck on 4/13/24.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
