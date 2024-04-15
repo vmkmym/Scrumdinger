898c35869cc8d2773d6d13a6644ae16f39aa227b
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-15 23:01:44 +0900

    feat: @State, @Binding을 이용하여 view간의 data flow를 관리
    - get 키워드로 해당 속성의 현재 값을 읽어 반환
    - set 키워드로 새로운 값을 속성에 할당
    - 뷰의 상태를 관리하고 데이터 흐름을 조절하는데 사용되는 @State, @Binding에서 상태 속성 래퍼는 뷰의 상태를 저장하고 변경함. 이를 사용하면 뷰가 다시 렌더링되고 사용자 인터랙션에 따라 상태를 업데이트할 수 있음. 바인딩은 속성을 다른 뷰에 바인딩하여 두 뷰 간에 데이터를 공유한다. 일반적으로 상위 뷰가 소유한 데이터를 하위 뷰에 전달하고 변경 사항을 반영하는 데 사용됨

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -13,6 +13,18 @@ struct DailyScrum: Identifiable {
     var title: String
     var attendees: [Attendee]
     var lengthInMinutes: Int
+    // 이중 정밀도 표현: 스크럼 길이를 조정할 슬라이더
+    // 계산된 속성을 사용하면 스크럼의 길이를 double값으로 검색하는 getter와 슬라이더가 변경될 때 해당 정수값을 업데이트하는 setter를 제공할 수 있음
+    var lengthInMinutesAsDouble: Double {
+        // 스크럼 회의 기간을 이중 값으로 반환하는 getter 제공
+        // 값을 반환하는 Swift 함수의 경우 return본문이 단일 표현식인 경우 키워드 생략 가능
+        get {
+            Double(lengthInMinutes)
+        }
+        set {
+            lengthInMinutes = Int(newValue)
+        }
+    }
     var theme: Theme
     
     // 기본값을 할당하는 초기화 프로그램 추가 -> 매개변수의 기본값을 정의하면 초기화 프로그램이나 함수를 호출할 때 해당 매개변수를 생략할 수 있음
@@ -35,6 +47,10 @@ extension DailyScrum {
             self.name = name
         }
     }
+    
+    static var emptyScrum: DailyScrum {
+            DailyScrum(title: "", attendees: [], lengthInMinutes: 5, theme: .sky)
+        }
 }
 
 extension DailyScrum {
--- Scrumdinger/Views/DetailEditView.swift
+++ Scrumdinger/Views/DetailEditView.swift
@@ -5,4 +5,59 @@
 //  Created by MorinBeck on 4/15/24.
 //
 
-import Foundation
+import SwiftUI
+// @State 속성을 정의한 뷰 내에서만 액세스하도록 속성을 private으로 선언
+
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                // TextField에 $구문을 사용하여 Binding을 생성한다
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(true)
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            // TextField에는 바인딩이 있으므로 값을 빈 문자열로 설정하면 텍스트 필드의 내용도 지워진다
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
