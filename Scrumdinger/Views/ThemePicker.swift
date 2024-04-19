f3494a0cbec46014bd63ecb4d4b2be4999d0a539
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-19 16:58:12 +0900

    feat: Add a theme picker
    - 사용자가 meeting view에서 color theme을 선택할 수 있는 custom interactive view를 만들기
    - theme view를 사용하여 앱의 모든 테마를 picker control에 나열함
    - 선택한 테마의 값을 parent view에 바인딩하는 변수에 저장한다
    - 그러면 theme view는 선택한 theme을 parent view에 전달한다

--- Scrumdinger/Views/ThemePicker.swift
+++ Scrumdinger/Views/ThemePicker.swift
@@ -4,5 +4,28 @@
 //
 //  Created by MorinBeck on 4/19/24.
 //
+// 상수 바인딩을 전달하여 미리 보기에서 테마 선택기를 초기화함. 상수(_:) 유형 메서드를 사용하여 하드코딩된 불변의 값에 대한 바인딩을 만들 수 있음. 상수 바인딩은 미리보기 또는 앱의 사용자 인터페이스를 프로토타이핑할 때 유용함
 
-import Foundation
+import SwiftUI
+
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
