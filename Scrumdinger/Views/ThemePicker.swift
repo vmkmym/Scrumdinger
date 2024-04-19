dddfdfac6a3ce2b988b9efc5d018ca4c776551d0
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-19 16:39:22 +0900

    feat: Create a new SwiftUI View file named ThemeView.swift in the Views group, Add a theme View
    - ThemeView는 목록의 cell로 사용하여 사용자에게 모든 테마를 표시하는 file
    - 사용자는 색상 테마를 선택해서 매일 스크럼을 차별화할 수 있으므로 테마의 구성 요소를 만듦
    - list view 구조에 같은 파일 안에 cell을 선언할 수 있지만 별도의 파일에 정의하면 향후 프로젝트에서 더 쉽게 재사용 가능

---
+++ Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by MorinBeck on 4/19/24.
+//
+
+import Foundation
---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by MorinBeck on 4/19/24.
+//
+
+import Foundation
