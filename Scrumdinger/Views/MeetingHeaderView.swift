baf12aeaa70f752fac9a1f3ede92dcb136cd1999
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-23 00:12:53 +0900

    feat: Extract the meeting footer, Trigger sound with AVFoundation
    - MeetingFooterView body에 HStack을 함수화한다.
    - 발표자 번호를 결정하는 비공개 속성 speakerNumber, 마지막 발표자인지 여부를 확인하는 비공개 속성  isLastSpeaker , 활성 발표자에 대한 정보를 반환하는 비공개 속성 speakerText
    
    - AVFoundation 프레임워크를 사용하면 Apple 플랫폼에서 시청각 미디어로 작업할 수 있음
    - 발표자의 시간이 종료되면 AVFoundation을 사용하여 오디오 피드백을 트리거

--- Scrumdinger/Views/MeetingHeaderView.swift
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -6,21 +6,19 @@
 //
 
 import SwiftUI
-    
+
 struct MeetingHeaderView: View {
-    let secondsElased: Int
+    let secondsElapsed: Int
     let secondsRemaining: Int
     let theme: Theme
-    
+
     private var totalSeconds: Int {
-        secondsElased + secondsRemaining
+        secondsElapsed + secondsRemaining
     }
-    
     private var progress: Double {
         guard totalSeconds > 0 else { return 1 }
         return Double(secondsElapsed) / Double(totalSeconds)
     }
-    
     private var minutesRemaining: Int {
         secondsRemaining / 60
     }
@@ -29,25 +27,22 @@ struct MeetingHeaderView: View {
         VStack {
             ProgressView(value: progress)
                 .progressViewStyle(ScrumProgressViewStyle(theme: theme))
-
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
                         .font(.caption)
-                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
-
+                    Label("\(secondsElapsed)", systemImage: "hourglass.tophalf.fill")
                 }
                 Spacer()
                 VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
                         .font(.caption)
-                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                    Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
                         .labelStyle(.trailingIcon)
-
                 }
             }
         }
-        .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+        .accessibilityElement(children: .ignore)
         .accessibilityLabel("Time remaining")
         .accessibilityValue("\(minutesRemaining) minutes")
         .padding([.top, .horizontal])
--- Scrumdinger/Views/ScrumProgressViewStyle.swift
+++ Scrumdinger/Views/ScrumProgressViewStyle.swift
@@ -5,4 +5,32 @@
 //  Created by MorinBeck on 4/22/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ScrumProgressViewStyle: ProgressViewStyle {
+    var theme: Theme
+
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(theme.accentColor)
+                .frame(height: 20.0)
+            if #available(iOS 15.0, *) {
+                ProgressView(configuration)
+                    .tint(theme.mainColor)
+                    .frame(height: 12.0)
+                    .padding(.horizontal)
+            } else {
+                ProgressView(configuration)
+                    .frame(height: 12.0)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProgressView(value: 0.4)
+        .progressViewStyle(ScrumProgressViewStyle(theme: .buttercup))
+        .previewLayout(.sizeThatFits)
+}
