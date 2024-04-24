ac73b3f7dad4a110295ea9ebe12aecb1e958a4a5
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-24 15:31:23 +0900

    feat: Load data on app launch
    road() 메서드를 사용하여 앱의 루트 보기가 화면에 표시될 때 데이터를 로드한다.
    ScrumsView에  task modifier를 추가합니다.
    task modifier는 비동기 함수 호출을 허용한다.
    do-catch 문을 사용하여 저장된 스크럼을 로드하거나 load()가 오류를 발생시키면 실행을 중지한다.
    (나중에 사용자에게 오류 정보를 표시할 것)

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -10,8 +10,8 @@ import SwiftUI
 @MainActor
 class ScrumStore: ObservableObject {
     @Published var scrums: [DailyScrum] = []
-
-
+    
+    
     private static func fileURL() throws -> URL {
         try FileManager.default.url(for: .documentDirectory,
                                     in: .userDomainMask,
@@ -19,7 +19,7 @@ class ScrumStore: ObservableObject {
                                     create: false)
         .appendingPathComponent("scrums.data")
     }
-
+    
     func load() async throws {
         let task = Task<[DailyScrum], Error> {
             let fileURL = try Self.fileURL()
@@ -31,5 +31,14 @@ class ScrumStore: ObservableObject {
         }
         let scrums = try await task.value
         self.scrums = scrums
+    }
+    
+    func save(scrums: [DailyScrum]) async throws {
+        let task = Task {
+            let data = try JSONEncoder().encode(scrums)
+            let outfile = try Self.fileURL()
+            try data.write(to: outfile)
+        }
+        _ = try await task.value
     }
 }
