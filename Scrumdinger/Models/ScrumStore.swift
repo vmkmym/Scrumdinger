2e1af7c6ad353df22ee59f4c7159742cc83f5553
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-24 15:06:00 +0900

    feat: Create a data store
    ScrumStore class를 만들고 SwiftUI 가져온다
    ObservableObject는 외부 모델 데이터를 SwiftUI 뷰에 연결하기 위한 클래스 제한 프로토콜
    
    게시된 스크럼 속성에 [DailyScrum] 유형의 @Published 스크럼을 추가한다.
    ObservableObject에는 @Published 속성 중 하나가 변경될 때 발생하는 objectWillChange 게시자가 포함됩니다.
    ScrumStore의 인스턴스를 관찰하는 모든 뷰는 스크럼 값이 변경되면 다시 렌더링된다.
    
    FileManager 클래스의 공유 인스턴스를 사용하여 현재 사용자의 문서 디렉터리 위치를 가져온다.

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by MorinBeck on 4/24/24.
+//
+
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+    }
+}
