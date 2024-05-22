9f19a17c0c84b87c51a6bdc5fb5b73a672046d16
상은 <71699054+vmkmym@users.noreply.github.com>
2024-05-23 01:33:56 +0900

    feat: #8 Create a history view
    - 회의 텍스트와 참석자를 표시하는 HistoryView

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -54,7 +54,8 @@ struct MeetingView: View {
         scrumTimer.stopScrum()
         speechRecognizer.stopTranscribing()
         isRecording = false
-        let newHistory = History(attendees: scrum.attendees)
+        let newHistory = History(attendees: scrum.attendees,
+                                         transcript: speechRecognizer.transcript)
         scrum.history.insert(newHistory, at: 0)
     }
 }
